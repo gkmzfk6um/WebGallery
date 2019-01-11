a4d351d76a8ccaec66a9bfa73909cbbbca236be6
Lukas Rahmn <lukas@rahmn.net>
2019-01-11 16:48:51 +0100

    Fix layout when scrollbar is present

--- js/main.js
+++ js/main.js
@@ -1,58 +1,64 @@
 const sum = as => as.reduce( (a,b) => a+b,0)
 function gallery() {
-
 	$('.gallery').each( function(index,elem){
-		width= $(this).innerWidth();
-		children=$(this).children().toArray();
-		//Calculate extra width
-		//child = $(children[0]).children('img');
-		//excess = $(children[0]).outerWidth(true)-child.outerWidth(true);
-		excess = $(children[0]).outerWidth(true)-$(children[0]).innerWidth();
-		
-		$('children').each(function() { $(this).css( {'width':'','height':''});});
-		row = function(elems,lastRow){
-			heights = $.map(elems ,e=> $(e).attr('data-height'));
-			min = Math.min(...heights);
-			widths =  $.map(elems, e=> $(e).attr('data-width') * min/ $(e).attr('data-height') );
+		var width = null;
+		var i = null;
+		//Loop to solve scrollbar appearing
+		while(width != $(this).innerWidth() &&  i++ < 2){
+			width= $(this).innerWidth();
+			children=$(this).children().toArray();
+			//Calculate extra width
+			//child = $(children[0]).children('img');
+			//excess = $(children[0]).outerWidth(true)-child.outerWidth(true);
+			excess = $(children[0]).outerWidth(true)-$(children[0]).innerWidth();
 			
-			if(!lastRow){
-				scale =  1.0001*sum(widths)/(width-elems.length*excess);
-			}
-			else{
-				scale=1.0;
-			}
-			widths = $.map(widths,e=>e/scale);
-			height = min/scale;
+			$('children').each(function() { $(this).css( {'width':'','height':''});});
+			row = function(elems,lastRow){
+				heights = $.map(elems ,e=> $(e).attr('data-height'));
+				min = Math.min(...heights);
+				widths =  $.map(elems, e=> $(e).attr('data-width') * min/ $(e).attr('data-height') );
+				
+				if(!lastRow){
+					scale =  1.0002*sum(widths)/(width-elems.length*excess);
+				}
+				else{
+					scale=1.0;
+				}
+				widths = $.map(widths,e=>e/scale);
+				height = min/scale;
 
 
-			if (scale < 1.0) {
-				return false;
+				if (scale < 1.0) {
+					return false;
+				}
+				else {
+					$(elems).each( function(i){
+						$(this).width(widths[i]);
+						$(this).height(height);
+					})
+					return true;
+				}
 			}
-			else {
-				$(elems).each( function(i){
-					$(this).width(widths[i]);
-					$(this).height(height);
-				})
-				return true;
+			
+			start=0;
+			end =1;
+			while(end <= children.length+1){
+				while(end <= children.length+1 && !row(children.slice(start,end),false)){
+					end++;
+				}
+				if (end > children.length+1)
+				{
+					row(children.slice(start,end),true);
+					break;
+				}
+				else{
+					start=end;
+					end++;
+				}
 			}
-		}
-		
-		start=0;
-		end =1;
-		while(end <= children.length+1){
-			while(end <= children.length+1 && !row(children.slice(start,end),false)){
-				end++;
-			}
-			if (end > children.length+1)
-			{
-				row(children.slice(start,end),true);
-				break;
-			}
-			else{
-				start=end;
-				end++;
-			}
-		}
+	}
+	
+		console.log('Image layout achived after ' + i + ' attempt(s).' );
 	})
 	
 }
@@ -83,4 +89,6 @@ function loadImages(){
 	console.log('Image loading started');
 }
 gallery();
-loadImages();
+$(document).ready(function(){
+	loadImages();
+})
